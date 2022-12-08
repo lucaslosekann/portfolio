834c60d5752342131f724dfc582ac8233e6efa23
Lucas Losekann <lucas1losekann@gmail.com>
2022-12-08 18:42:04 +0000

    Changed about font and added hover on scroll buttons

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -8,11 +8,13 @@ export default function About({}: Props) {
       <h3 className='uppercase tracking-[10px] text-xl'>About me</h3>
       <div className='flex flex-col sm:flex-row items-center flex-1 pt-4 md:px-8'>
         <img src="https://github.com/lucaslosekann.png" alt="Lucas's Photo" className='object-cover rounded-full sm:rounded-lg w-5/6 sm:w-1/2 md:h-5/6 md:w-2/5' />
-        <article className='mt-2 pb-5 sm:text-left sm:pl-5 md:px-5 md:pl-10'>
-          <h4 className='text-white tracking-wider text-xl mb-2 md:text-3xl md:font-bold md:mb-5'>I'm Lucas Losekann Rosa,</h4>
-          <span className='md:text-xl text-neutral-200'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab quidem, a eius commodi molestias dignissimos cupiditate, explicabo iure iste sunt cum pariatur. Illo, dolores illum eius quo hic maxime veniam!
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab quidem, a eius commodi molestias dignissimos cupiditate, explicabo iure iste sunt cum pariatur. Illo, dolores illum eius quo hic maxime veniam!
+        <article className='mt-2 pb-5 sm:text-left sm:pl-5 md:px-5 md:pl-10 text-justify'>
+          <h4 className='text-white tracking-wider text-xl mb-2 md:text-3xl text-center md:font-semibold md:mb-5'>I&apos;m Lucas Losekann Rosa,</h4>
+          <span className='md:text-xl text-neutral-200 font-light'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem sunt sit incidunt dolorem amet labore necessitatibus. Autem asperiores assumenda eius ipsa, est possimus facilis, consequatur mollitia at cum, quibusdam velit!
+          Voluptate facere cumque illum ea nihil commodi et amet earum voluptates aspernatur vel in saepe impedit veritatis quas, a cupiditate delectus adipisci eius fugiat. Laboriosam sequi tempore recusandae rerum quasi?
+          Sed id officiis qui distinctio, voluptate doloribus, ullam perspiciatis maiores ducimus veritatis, minus deleniti alias laudantium dolore facere nihil debitis. Obcaecati unde debitis dolor odio! Cumque, veritatis enim? Assumenda, ipsa!
+          Facilis laboriosam quae maiores corporis pariatur temporibus culpa in repellat magnam, voluptas animi numquam architecto aspernatur consectetur placeat, beatae mollitia! Voluptas corrupti dolores repellat veniam, fugit nulla assumenda blanditiis ipsam.
           </span>
         </article>
       </div>
--- components/HorizontalDraggable/index.tsx
+++ components/HorizontalDraggable/index.tsx
@@ -52,13 +52,13 @@ export default function HorizontalDraggable({ children }: Props) {
         }
       }
     }
-  }, [])
+  }, [children])
 
   return (
     <div ref={draggableDiv} className='flex-1 customscroll scroll-smooth flex overflow-y-hidden flex-row overflow-x-scroll snap-x snap-mandatory'
       onScroll={scrollHandler}>
-        <svg onClick={leftHandler} className="hidden md:block w-16 h-16 cursor-pointer absolute my-auto top-0 bottom-0 left-0 z-30 2xl:left-32" clipRule="evenodd" fillRule="evenodd" fill='currentColor' strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591zm-.289 7.563v-5.446l-3.522 2.719z" fillRule="nonzero" /></svg>
-        <svg onClick={rightHandler} className="2xl:right-32 hidden md:block w-16 h-16 cursor-pointer absolute my-auto top-0 bottom-0 right-0 z-30" clipRule="evenodd" fillRule="evenodd" fill='currentColor' strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591zm.289 7.563v-5.446l3.522 2.719z" fillRule="nonzero" /></svg>
+        <svg onClick={leftHandler} className="text-gray-600 hover:text-inherit hidden md:block w-16 h-16 cursor-pointer absolute my-auto top-0 bottom-0 left-0 z-30 2xl:left-32" clipRule="evenodd" fillRule="evenodd" fill='currentColor' strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591zm-.289 7.563v-5.446l-3.522 2.719z" fillRule="nonzero" /></svg>
+        <svg onClick={rightHandler} className="text-gray-600 hover:text-inherit 2xl:right-32 hidden md:block w-16 h-16 cursor-pointer absolute my-auto top-0 bottom-0 right-0 z-30" clipRule="evenodd" fillRule="evenodd" fill='currentColor' strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591zm.289 7.563v-5.446l3.522 2.719z" fillRule="nonzero" /></svg>
       {children}
     </div>
   )
