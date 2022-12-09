7d3a6c515a9189cb2fec1ad6524519ef0b2da040
Lucas Losekann <50055600+lucaslosekann@users.noreply.github.com>
2022-12-09 16:29:21 -0300

    Chenged people to everybody

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -7,7 +7,7 @@ type Props = {}
 export default function Hero({ }: Props) {
   const [text] = useTypewriter({
     words: [
-      'Hello, people call me Lucas', 'Fella-That-Codes.tsx', '<AndDrinksALotOfCoffee />'
+      'Hello, everybody calls me Lucas', 'Fella-That-Codes.tsx', '<AndDrinksALotOfCoffee />'
     ],
     loop: true,
     delaySpeed: 2000
